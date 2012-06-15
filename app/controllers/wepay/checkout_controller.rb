27a8cced3de1006755e47dea7dbc3a4b1459f479
Gilbert <gilbertbgarza@gmail.com>
2012-06-15 15:24:03 -0500

    Fixed a bug with #attributes having keys as strings instead of symbols

--- app/controllers/wepay/checkout_controller.rb
+++ app/controllers/wepay/checkout_controller.rb
@@ -10,13 +10,15 @@ class Wepay::CheckoutController < Wepay::ApplicationController
 
     if record.present?
       wepay_gateway = WepayRails::Payments::Gateway.new
+
       if record.checkout_id.present?
         checkout = wepay_gateway.lookup_checkout(record.checkout_id)
       else
         checkout = wepay_gateway.lookup_preapproval(record.preapproval_id)
       end
-      checkout.delete_if {|k,v| !record.attributes.include? k}
+      checkout.delete_if {|k,v| !record.attributes.include? k.to_s}
       record.update_attributes(checkout)
+
       if params[:preapproval_id]
         redirect_to "#{wepay_gateway.configuration[:after_checkout_redirect_uri]}?preapproval_id=#{params[:preapproval_id]}"
       else
